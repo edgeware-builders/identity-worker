f2fae2ad3e1a9601621c6f25919ad1f2407af641
Jake Naviasky <jake@commonwealth.im>
2020-10-27 14:23:50 -0400

    Add started verify code.

--- pallets/worker/src/lib.rs
+++ pallets/worker/src/lib.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(feature = "std")]
+use serde::{Serialize, Deserialize};
+
 use frame_system::{
 	self as system,
 	offchain::{
@@ -10,13 +13,18 @@ use frame_support::{
 	debug, decl_module, decl_storage, decl_event,
 	traits::Get,
 };
-use sp_core::crypto::KeyTypeId;
 use sp_runtime::{
+	RuntimeDebug,
 	transaction_validity::{
 		ValidTransaction, TransactionValidity, TransactionSource,
 		TransactionPriority,
 	},
+	offchain::{
+		http, Duration
+	}
 };
+use codec::{Encode, Decode};
+use sp_std::vec::Vec;
 
 /// This pallet's configuration trait
 pub trait Trait: CreateSignedTransaction<Call<Self>> {
@@ -31,31 +39,39 @@ pub trait Trait: CreateSignedTransaction<Call<Self>> {
 	// Configuration parameters
 
 	/// A grace period after we send transaction.
-	///
-	/// To avoid sending too many transactions, we only attempt to send one
-	/// every `GRACE_PERIOD` blocks. We use Local Storage to coordinate
-	/// sending between distinct runs of this offchain worker.
 	type GracePeriod: Get<Self::BlockNumber>;
 
 	/// Number of blocks of cooldown after unsigned transaction is included.
-	///
-	/// This ensures that we only accept unsigned transactions once, every `UnsignedInterval` blocks.
 	type UnsignedInterval: Get<Self::BlockNumber>;
 
 	/// A configuration for base priority of unsigned transactions.
-	///
-	/// This is exposed so that it can be tuned for particular runtime, when
-	/// multiple pallets send unsigned transactions.
 	type UnsignedPriority: Get<TransactionPriority>;
 }
 
+/// A pending identity verification.
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct PendingVerification<AccountId> {
+	/// The location of the verification page
+	url: Vec<u8>,
+	/// The account submitting the verification request
+	submitter: AccountId,
+	/// The target account to verify
+	target: AccountId,
+}
+
 decl_storage! {
-	trait Store for Module<T: Trait> as WorkerModule {}
+	trait Store for Module<T: Trait> as WorkerModule {
+		/// Verification submitted and waiting for approval. FIFO queue.
+		PendingVerifications get(fn pending_verifications): Vec<PendingVerification<T::AccountId>>;
+	}
 }
 
 decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
 		NewHeader(u32, AccountId),
+		/// An identity verification was processed, and was approved (true/false)
+		VerificationProcessed(AccountId, bool),
 	}
 );
 
@@ -68,32 +84,54 @@ decl_module! {
 		fn deposit_event() = default;
 
 		/// Offchain Worker entry point.
-		///
-		/// By implementing `fn offchain_worker` within `decl_module!` you declare a new offchain
-		/// worker.
-		/// This function will be called when the node is fully synced and a new best block is
-		/// succesfuly imported.
-		/// Note that it's not guaranteed for offchain workers to run on EVERY block, there might
-		/// be cases where some blocks are skipped, or for some the worker runs twice (re-orgs),
-		/// so the code should be able to handle that.
-		/// You can use `Local Storage` API to coordinate runs of the worker.
 		fn offchain_worker(block_number: T::BlockNumber) {
-			// It's a good idea to add logs to your offchain workers.
-			// Using the `frame_support::debug` module you have access to the same API exposed by
-			// the `log` crate.
-			// Note that having logs compiled to WASM may cause the size of the blob to increase
-			// significantly. You can use `RuntimeDebug` custom derive to hide details of the types
-			// in WASM or use `debug::native` namespace to produce logs only when the worker is
-			// running natively.
 			debug::native::info!("Hello World from offchain workers!");
 
-			// Since off-chain workers are just part of the runtime code, they have direct access
-			// to the storage and other included pallets.
-			//
 			// We can easily import `frame_system` and retrieve a block hash of the parent block.
 			let parent_hash = <system::Module<T>>::block_hash(block_number - 1.into());
 			debug::debug!("Current block: {:?} (parent hash: {:?})", block_number, parent_hash);
+
+			// Grab the next item off the pending verifications queue
+			let verification_opt = PendingVerifications::<T>::mutate(|v| { v.pop() });
+			let verification = match verification_opt {
+				Some(v) => v,
+				None => return, // do nothing if no pending verifications
+			};
+			Self::verify(verification);
 		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+	fn verify(verification: PendingVerification<T::AccountId>) -> Result<bool, http::Error> {
+		let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
+		let url_str = sp_std::str::from_utf8(&verification.url).map_err(|_| {
+			debug::warn!("No UTF8 url");
+			http::Error::Unknown
+		})?;
+		let request = http::Request::get(url_str);
+		// TODO: set headers, oauth stuff
+		let pending = request
+			.deadline(deadline)
+			.send()
+			.map_err(|_| http::Error::IoError)?;
+
+		let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+		if response.code != 200 {
+			debug::warn!("Unexpected status code: {}", response.code);
+			return Err(http::Error::Unknown);
+		}
+
+		// Collect body and create a str slice.
+		let body = response.body().collect::<Vec<u8>>();
+		let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+			debug::warn!("No UTF8 body");
+			http::Error::Unknown
+		})?;
+		debug::debug!("Got response body: {:?}", body_str);
+
+		// TODO: verify fetched response
+		Ok(true)
 	}
 }
 
@@ -102,10 +140,6 @@ impl<T: Trait> frame_support::unsigned::ValidateUnsigned for Module<T> {
 	type Call = Call<T>;
 
 	/// Validate unsigned call to this module.
-	///
-	/// By default unsigned transactions are disallowed, but implementing the validator
-	/// here we make sure that some particular calls (the ones produced by offchain worker)
-	/// are being whitelisted and marked as valid.
 	fn validate_unsigned(
 		_source: TransactionSource,
 		_call: &Self::Call,
